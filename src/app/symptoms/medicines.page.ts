ef8e039ccb329a9cd817711e929216a18a164992
Alinson Lopez <alinsonl11@gmail.com>
2024-05-26 19:27:17 -0400

    symptoms added

---
+++ src/app/symptoms/medicines.page.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MedicinesService, Medicine } from 'src/app/services/medicines.service';
+import { SymptomService, Symptom } from 'src/app/services/symptoms.service';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './medicines.page.html',
+  styleUrls: ['./medicines.page.scss', '../../assets/css/general.scss'],
+  standalone: true,
+  imports: [RouterLink, IonicModule, CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class MedicinesPage implements OnInit {
+  medicines: Medicine[] = [];
+  symptoms: Symptom[] = [];
+  chunkedMedications: Medicine[][] = [];
+  slideOpts = {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    effect: 'slide'
+  };
+
+  constructor(
+    private medicineService: MedicinesService,
+    private symptomService: SymptomService
+  ) {}
+
+  ngOnInit() {
+    this.medicineService.getMedicines().subscribe((medicines) => {
+      this.medicines = medicines;
+      this.symptomService.getSymptoms().subscribe((symptoms) => {
+        this.symptoms = symptoms;
+        this.mapSymptomsToMedicines();
+        this.chunkMedications();
+      });
+    });
+  }
+
+  mapSymptomsToMedicines() {
+    const symptomMap = new Map(this.symptoms.map((symptom) => [symptom.id, symptom.nombre]));
+
+    this.medicines.forEach((medicine) => {
+      medicine.sintomas_asociados_nombres = medicine.sintomas_asociados
+        .map((symptomId) => symptomMap.get(symptomId))
+        .filter((symptomName): symptomName is string => symptomName !== undefined);
+    });
+  }
+
+  chunkMedications() {
+    this.chunkedMedications = [];
+    for (let i = 0; i < this.medicines.length; i += 2) {
+      this.chunkedMedications.push(this.medicines.slice(i, i + 2));
+    }
+  }
+}
--- src/app/symptoms/symptoms.page.ts
+++ src/app/symptoms/symptoms.page.ts
@@ -1,22 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-import {IonicModule} from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { SymptomService, Symptom } from 'src/app/services/symptoms.service';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
-  selector: 'app-symptoms',
+  selector: 'app-main',
   templateUrl: './symptoms.page.html',
   styleUrls: ['./symptoms.page.scss', '../../assets/css/general.scss'],
   standalone: true,
-  imports: [RouterLink, IonicModule]
+  imports: [RouterLink, IonicModule, CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SymptomsPage implements OnInit {
+  symptoms: Symptom[] = [];
+  chunkedSymptoms: Symptom[][] = [];
+  currentChunk = 0;
 
-  constructor() { }
+  constructor(
+    private symptomService: SymptomService
+  ) {}
 
   ngOnInit() {
+    this.symptomService.getSymptoms().subscribe((symptoms) => {
+      this.symptoms = symptoms;
+      this.chunkSymptoms();
+    });
   }
 
+  chunkSymptoms() {
+    this.chunkedSymptoms = [];
+    for (let i = 0; i < this.symptoms.length; i += 2) {
+      this.chunkedSymptoms.push(this.symptoms.slice(i, i + 2));
+    }
+  }
+
+  prev() {
+    if (this.currentChunk > 0) {
+      this.currentChunk--;
+    }
+  }
+
+  next() {
+    if (this.currentChunk < this.chunkedSymptoms.length - 1) {
+      this.currentChunk++;
+    }
+  }
 }
