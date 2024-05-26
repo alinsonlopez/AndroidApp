0db9ba0cff3c88f161cf32592d6112ab8e563bcb
Alinson Lopez <alinsonl11@gmail.com>
2024-05-26 13:21:10 -0400

    new changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,13 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-<<<<<<< HEAD
-  // { path: '', redirectTo: 'medicines', pathMatch: 'full' },
+
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
 
   {
-    path: 'main',
-    loadComponent: () => import('./main/main.page').then(m => m.MainPage)
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then(m => m.HomePage)
   },
-=======
-
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-
-  {path: 'home', 
-  loadComponent: ()=> import('./home/home.page').then(m=>m.HomePage)  },
->>>>>>> 1bbdc6b
   {
     path: 'medicines',
     loadComponent: () => import('./medicines/medicines.page').then(m => m.MedicinesPage)
